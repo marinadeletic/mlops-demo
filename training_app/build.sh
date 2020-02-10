ea41d650f51d814dc74ebc67c91a2b1f9ed1a481
marinadeletic <marina@deletic.org>
2020-02-10 06:19:28 +0000

    building images

--- training_app/build.sh
+++
@@ -1,6 +0,0 @@
-
-IMAGE_NAME='trainer_image'
-IMAGE_TAG='latest'
-IMAGE_URI='gcr.io/{}/{}:{}'.format(PROJECT_ID, IMAGE_NAME, IMAGE_TAG)
-
-!gcloud builds submit --tag $IMAGE_URI $TRAINING_APP_FOLDER
